ff074403dcc8ec08ce89bd11ae916684264f74c0
Andrii Ternovyi <andriternovy@yandex.ru>
2013-08-22 23:07:10 +0300

    new

---
+++ test/test/wqdqViewController.m
@@ -0,0 +1,38 @@
+//
+//  wqdqViewController.m
+//  test
+//
+//  Created by Andrii Ternovyi on 22.08.13.
+//  Copyright (c) 2013 dd. All rights reserved.
+//
+
+#import "wqdqViewController.h"
+
+@interface wqdqViewController ()
+
+@end
+
+@implementation wqdqViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
