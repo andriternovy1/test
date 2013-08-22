6db15182c2e168e03673cc6fe6f9b19a0a6e960f
Andrii Ternovyi <andriternovy@yandex.ru>
2013-08-22 23:00:36 +0300

    firstCommit

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Andrii Ternovyi on 22.08.13.
+//  Copyright (c) 2013 dd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
